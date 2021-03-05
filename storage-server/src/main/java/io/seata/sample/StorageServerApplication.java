9212e007c3f08aa5aaca81fb1864c392051d6a69
lishi <lishi2@codemao.cn>
2021-03-06 00:31:54 +0800

    account 新增fegin

--- storage-server/src/main/java/io/seata/sample/StorageServerApplication.java
+++ storage-server/src/main/java/io/seata/sample/StorageServerApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 库存服务
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("io.seata.sample.dao")
+@EnableEurekaClient
+@EnableFeignClients
 public class StorageServerApplication {
 
 	public static void main(String[] args) {
